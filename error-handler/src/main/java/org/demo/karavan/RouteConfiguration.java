a8bd4872fdc9ed9cf7767b5510d54efc7fb8c6fd
Marat Gubaidullin <marat.gubaidullin@gmail.com>
2023-07-16 21:19:38 -0400

    routeConfigurationId

--- error-handler/src/main/java/org/demo/karavan/RouteConfiguration.java
+++ error-handler/src/main/java/org/demo/karavan/RouteConfiguration.java
@@ -9,9 +9,17 @@ public class RouteConfiguration extends RouteConfigurationBuilder {
 
     @Override
     public void configuration() throws Exception {
+
+//      Default route configuration
         routeConfiguration().errorHandler(
+                deadLetterChannel("amqp:errors-queue") // produce message to Artemis
+                        .maximumRedeliveries(5)
+                        .redeliveryDelay(1000)
+                        .useOriginalMessage()  // set if you want to have original Exchange payload
+        );
+//      Special route configuration
+        routeConfiguration().id("specialConfig").errorHandler(
                 deadLetterChannel("direct:error") // send to route
-//                deadLetterChannel("amqp:errors-queue") // produce message to Artemis
                         .maximumRedeliveries(5)
                         .redeliveryDelay(1000)
                         .useOriginalMessage()  // set if you want to have original Exchange payload
